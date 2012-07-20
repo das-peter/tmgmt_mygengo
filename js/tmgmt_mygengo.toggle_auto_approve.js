bfc363b31c479c10d8ba91243a59a46311e77ddc
divamys <divamys@128652.no-reply.drupal.org>
2012-07-20 09:18:52 +0200

    Issue #1689208 by blueminds: Added reject process.

--- js/tmgmt_mygengo.toggle_auto_approve.js
+++ js/tmgmt_mygengo.toggle_auto_approve.js
@@ -2,13 +2,14 @@
 
   Drupal.behaviors.tmgmt_mygengo = {
     attach: function (context, settings) {
+
+      if ($('#edit-settings-use-sandbox').is(':checked')) {
+        $('.mygengo-auto-approve input').attr('checked', 1);
+      }
+
       $('.mygengo-use-sandbox input').click(function() {
         if ($('#edit-settings-use-sandbox').is(':checked')) {
-          $('.mygengo-auto-approve input').attr('checked', $('.mygengo-use-sandbox input').attr('checked'));
-          $('.mygengo-auto-approve input').attr('disabled', 1);
-        }
-        else {
-          $('.mygengo-auto-approve input').attr('disabled', 0);
+          $('.mygengo-auto-approve input').attr('checked', 1);
         }
       });
     }
